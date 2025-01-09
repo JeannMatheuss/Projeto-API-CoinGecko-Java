7cc769c7ff055e5ee8f65fd0a3642183b12d3610
Jean <jeanmatheus0612@gmail.com>
2025-01-09 20:14:07 -0300

    Criando o scanner para receber a criptomoeda

--- src/Principal.java
+++ src/Principal.java
@@ -4,10 +4,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
-        
+        Scanner leitura = new Scanner (System.in);
+        System.out.println("Digite uma criptomoeda para saber a cotação atual: ");
+        var criptomoeda = leitura.nextLine();
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
