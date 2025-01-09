ee5c4d2aaaa14ca72cb79e730bddd2cbe027a9e6
Jean <jeanmatheus0612@gmail.com>
2025-01-09 20:11:38 -0300

    Adicionando a classe HTTP response

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,20 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("http://foo.com/"))
             .build();
+
+        HttpResponse<String> response = client
+            .send(request, BodyHandlers.ofString());
     }
 }
